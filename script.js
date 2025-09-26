67ff5ee60128fe6bc1b796a4f4b392e03e876e4f
Eric <mateieric66@gmail.com>
2025-09-26 04:03:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-
-  const menuToggle = document.getElementById("menu-toggle");
-  const nav = document.getElementById("nav");
-
-  menuToggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
-    menuToggle.classList.toggle("active");
-    menuToggle.innerHTML = menuToggle.classList.contains("active") ? "×" : "&#9776;";
-  });
-
-  const toggle = document.getElementById('theme-toggle');
+const toggle = document.getElementById('theme-toggle');
 toggle.addEventListener('click', () => {
   document.body.classList.toggle('dark');
 });
